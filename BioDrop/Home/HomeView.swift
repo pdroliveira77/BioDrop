4df824c870494f0e1d53d28ae034c9705028747d
pdroliveira77 <pho.alves@gmail.com>
2026-06-05 18:03:09 -0300

    - Criado BottomNavigation e Home - Separado Components da pasta Extensions

---
+++ BioDrop/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BioDrop
+//
+//  Created by Pedro Oliveira on 05/06/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
